3b457c8787749fd011f33e800270126688e18741
Attila-Fejer <attila@fejer.io>
2025-03-09 15:35:16 +0100

    chore: make oh my zsh and p10k installation files scripts instead of chezmoi script templates

--- home/dot_dotfiles/_zsh/_10_oh-my-zsh/run_once_install.sh
+++ home/dot_dotfiles/_zsh/_10_oh-my-zsh/run_once_install.sh
@@ -1,9 +1,9 @@
-{{- if lookPath "zsh" }}
 #!/usr/bin/env bash
+
+which zsh || exit 0
 
 oh_my_zsh_folder="${ZSH:-$HOME/.oh-my-zsh}"
 
 if [ ! -f $oh_my_zsh_folder/oh-my-zsh.sh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-{{ end -}}
--- home/dot_dotfiles/_zsh/_20_p10k/run_once_install.sh
+++ home/dot_dotfiles/_zsh/_20_p10k/run_once_install.sh
@@ -1,9 +1,9 @@
-{{- if lookPath "zsh" }}
 #!/usr/bin/env bash
+
+which zsh || exit 0
 
 p10k_folder="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 if [ ! -d "$p10k_folder" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_folder"
 fi
-{{ end -}}
