bdb47b140186d99ce068f433e9f55784c62bfbac
Attila-Fejer <attila@fejer.io>
2025-03-09 16:52:37 +0100

    chore: separate container and pc init files

--- init.sh
+++ init.sh
@@ -36,26 +36,53 @@ check_platform() {
 install_if_not_available() {
   app_name=$1
   install_command="${@:2}"
-  which -s $app_name || $install_command
+  if ! command -v $app_name; then
+    $install_command
+  fi
+}
+
+try_install_brew() {
+  install_if_not_available brew /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+install_chezmoi_binary() {
+  sh -c "$(curl -fsLS get.chezmoi.io)" || \
+  sh -c "$(wget -qO- get.chezmoi.io)" || \
+  halt "Failed to install chezmoi"
+  export PATH="$PATH:./bin"
+}
+
+install_chezmoi() {
+  if command -v brew; then
+    brew install chezmoi
+  else
+    install_chezmoi_binary
+  fi
 }
 
 init_chezmoi_config() {
   mkdir -p $HOME/.config/chezmoi
-  cat >> $HOME/.config/chezmoi/chezmoi.toml << 'EOF'
+  cat >> $HOME/.config/chezmoi/chezmoi.toml << EOF
 [onepassword]
-    prompt = false
+  prompt = false
 [data]
-  {{- $physical_computer := promptBoolOnce . "physical_computer" "Is this a physical computer for daily driving (true) or a container (false)?" -}}
+  physical_computer = $physical_computer
 
 EOF
 }
 
-check_platform
-install_if_not_available brew /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install chezmoi
-chezmoi init
-init_chezmoi_config
-chezmoi init fanatixan
-chezmoi doctor
-chezmoi apply
-chezmoi update
+debug() {
+  echo "########## Running ${@}"
+  ${@}
+}
+
+echo "\$ZSH:" $ZSH
+debug check_platform
+debug try_install_brew
+debug install_chezmoi
+# debug chezmoi init
+debug init_chezmoi_config
+debug chezmoi init fanatixan
+debug chezmoi doctor
+debug chezmoi apply
+debug chezmoi update
