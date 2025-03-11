778a656d688b5922fcb5594a5815ab929101b747
Attila-Fejer <attila@fejer.io>
2025-03-11 21:08:51 +0100

    feat: make SSH and git rely solely on 1Password for SSH keys

--- home/dot_dotfiles/1password/rc.sh
+++ home/dot_dotfiles/1password/rc.sh
@@ -1,1 +1,5 @@
+{{- if eq .chezmoi.os "darwin" }}
 export SSH_AUTH_SOCK=$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+{{- else if eq .chezmoi.os "linux" }}
+export SSH_AUTH_SOCK=$HOME/.1Password/agent.sock
+{{- end }}
