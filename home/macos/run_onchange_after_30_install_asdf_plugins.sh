e5c17edbcaef791d2051816435603c11c16b8e51
Attila-Fejer <attila@fejer.io>
2025-03-22 14:55:07 +0100

    fix: fix asdf plugin add command

--- home/macos/run_onchange_after_30_install_asdf_plugins.sh
+++ home/macos/run_onchange_after_30_install_asdf_plugins.sh
@@ -5,19 +5,19 @@ java_version() {
   asdf list all java | grep $version | tail -1
 }
 
-asdf add plugin java
+asdf plugin add java
 asdf install java $(java_version temurin-11)
 asdf install java $(java_version temurin-17)
 asdf install java $(java_version temurin-21)
 asdf set -u java $(java_version temurin-21)
 
-asdf add plugin nodejs
+asdf plugin add nodejs
 asdf install nodejs latest:18
 asdf install nodejs latest:20
 asdf install nodejs latest:22
 asdf set -u nodejs $(asdf latest nodejs 22)
 
-asdf add plugin python
+asdf plugin add python
 asdf install python latest:3.7
 asdf install python latest
 asdf set -u python $(asdf latest python)
