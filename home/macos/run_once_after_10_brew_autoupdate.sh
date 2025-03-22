43fd50ec970be7f589a6a494a6b5ef0ad87cb7c6
Attila-Fejer <attila@fejer.io>
2025-03-22 14:53:26 +0100

    fix: create launch agents folder for brew autoupdate

--- home/macos/run_once_after_10_brew_autoupdate.sh
+++ home/macos/run_once_after_10_brew_autoupdate.sh
@@ -9,4 +9,5 @@ brew tap domt4/autoupdate
 # making sudo possible
 brew install pinentry-mac
 # run it every day and on system boot, show credentials prompt if needed
+mkdir -p $HOME/Library/LaunchAgents
 brew autoupdate start 86400 --cleanup --upgrade --immediate --sudo
