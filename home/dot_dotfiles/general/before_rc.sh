9c92adbae9ea7ecbeb3a09af6a545e34623f7e96
Attila-Fejer <attila@fejer.io>
2025-04-08 18:02:36 +0200

    chore: load user paths for both interactive an non-interactive shells

--- home/dot_dotfiles/general/before_rc.sh
+++ home/dot_dotfiles/general/before_rc.sh
@@ -1,12 +1,2 @@
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
-fi
-
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
