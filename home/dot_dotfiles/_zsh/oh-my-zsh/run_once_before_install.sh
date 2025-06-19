5b7bd542cc707ecba1551ce073f53effe3e01ba7
Attila-Fejer <attila@fejer.io>
2025-06-19 15:49:52 +0200

    feat: unattanded oh my zsh install

--- home/dot_dotfiles/_zsh/oh-my-zsh/run_once_before_install.sh
+++ home/dot_dotfiles/_zsh/oh-my-zsh/run_once_before_install.sh
@@ -5,5 +5,5 @@ which zsh || exit 0
 oh_my_zsh_folder="${ZSH:-$HOME/.oh-my-zsh}"
 
 if [ ! -f $oh_my_zsh_folder/oh-my-zsh.sh ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
